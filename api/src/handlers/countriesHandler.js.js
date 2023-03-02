fdad2b85c1172791be6adc916fd69baef7d9bcf6
LMCT04 <lucianochiritotorres@gmail.com>
2023-03-02 13:47:31 -0300

    ErrorGETname=null

--- api/src/handlers/countriesHandler.js.js
+++ api/src/handlers/countriesHandler.js.js
@@ -3,9 +3,18 @@ const { allCountries, countryByName, countryById } = require('../controllers/cou
 const getCountries = async (req, res) => {
     const {name} = req.query;
     try {
+        const countries = await allCountries()
+        if(name){
+            const result = await countryByName(name)
+            if(result.length) {
+                return res.status(200).json(result)
+            }
+            return res.status(400).json(`${name} not exist`)
+        }
+        return res.status(200).json(countries)
         //const name = req.query;
-        const result = name ? await countryByName(name) : await allCountries();
-        res.status(200).json(result)
+        //const result = name ? await countryByName(name) : res.status(400).send(`${name} not exist`);
+        //res.status(200).json(result)
     } catch(error){
         res.status(400).json({error: error.message})
     }
